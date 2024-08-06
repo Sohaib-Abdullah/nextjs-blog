2af19b205b81fbbad8cbfc803d2d1d84267d9e4f
Sohaib-Abdullah <sohaibsohaib77@gmail.com>
2024-08-06 14:54:06 +0500

    prisma-adapter-changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,15 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import prisma from "@/lib/prismadb";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(
+    prisma
+  ) as unknown as import("next-auth/adapters").Adapter, // Type compatibility fix
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
